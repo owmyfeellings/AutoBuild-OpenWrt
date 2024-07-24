a7cbe1b788f99f9b9661ef60c18884b02a5ecf50
owmyfeellings <46664129+owmyfeellings@users.noreply.github.com>
2024-07-25 00:21:05 +0800

    update

--- customize.sh
+++ customize.sh
@@ -13,9 +13,13 @@ sed -i '/luci.main.mediaurlbase/d' ./package/feeds/luci/luci-theme-design/root/e
 sed -i '/luci.main.mediaurlbase/d' ./package/feeds/infinityfreedom_ng/luci-theme-infinityfreedom-ng/files/11_luci-theme-infinityfreedom-ng
 popd
 
-#pushd ./openwrt/feeds/packages
-#git apply $(find $HOME/ -name "patches" -type d)/*.patch
-#popd
+pushd ./openwrt/package/feeds/lienol/v2ray-geodata
+sed -i 's#'$(grep -E -a --line-buffered 'GEOIP_VER:=' ./Makefile)'#GEOIP_VER:=latest#g' ./Makefile
+sed -i 's#'$(grep -E -a --line-buffered 'GEOSITE_VER:=' ./Makefile)'#GEOSITE_VER:=latest#g' ./Makefile
+sed -i 's#'$(grep -E -a --line-buffered 'HASH:=' ./Makefile)'#HASH:=skip#g' ./Makefile
+sed -i 's#https://github.com/v2fly/geoip/releases/download/$(GEOIP_VER)/#https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/#g' ./Makefile
+sed -i 's#https://github.com/v2fly/domain-list-community/releases/download/$(GEOSITE_VER)/#https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/#g' ./Makefile
+popd
 
 pushd ./my-feeds
 sed -i '/luci.main.mediaurlbase/d' $(find $(find ./ -name "luci-theme-atmaterial-ColorIcon" -type d) -name "30_luci-theme-atmaterial_ci" -type f)
