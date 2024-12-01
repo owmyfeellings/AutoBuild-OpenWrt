e7248eaf55b9cba3746b2a3f49ba50d1f6f42e60
owmyfeellings <46664129+owmyfeellings@users.noreply.github.com>
2024-12-01 18:37:30 +0800

    update

--- customize.sh
+++ customize.sh
@@ -6,21 +6,11 @@
 # Youtube Channel: https://goo.gl/fvkdwm 
 #=================================================
 
-echo "home dir is: "$HOME
-echo "current dir is: "$PWD
-#tree -a -C -f ./openwrt
-
 pushd ./openwrt
 sed -i 's/192.168.1.1/192.168.10.2/g' ./package/base-files/files/bin/config_generate
 sed -i 's#$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.##g' ./package/lean/default-settings/files/zzz-default-settings
-sed -i '/luci.main.mediaurlbase/d' ./package/feeds/luci/luci-theme-design/root/etc/uci-defaults/30_luci-theme-design
-sed -i '/luci.main.mediaurlbase/d' ./package/feeds/infinityfreedom_ng/luci-theme-infinityfreedom-ng/files/11_luci-theme-infinityfreedom-ng
 popd
 
 pushd ./my-feeds
-sed -i '/luci.main.mediaurlbase/d' $(find $(find ./ -name "luci-theme-argon" -type d) -name "90_luci-theme-argon" -type f)
-#sed -i '/luci.main.mediaurlbase/d' $(find $(find ./ -name "luci-theme-edge" -type d) -name "30_luci-theme-edge" -type f)
-#sed -i '/luci.main.mediaurlbase/d' $(find $(find ./ -name "luci-theme-opentopd" -type d) -name "30_luci-theme-opentopd" -type f)
-#sed -i '/luci.main.mediaurlbase/d' $(find $(find ./ -name "luci-theme-neobird" -type d) -name "30_luci-theme-neobird" -type f)
-#sed -i '/luci.main.mediaurlbase/d' $(find $(find ./ -name "luci-theme-opentomcat" -type d) -name "30_luci-theme-opentomcat" -type f)
+sed -i '/luci.main.mediaurlbase/d' $(find $(find ./ -name "luci-theme-kucat" -type d) \( -name "30_luci-kucat" -or -name "30_luci-kuacat" \) -type f)
 popd
