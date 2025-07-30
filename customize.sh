5ea4ae281001e984f336c0c8dfcaab4bb8dd7977
owmyfeellings <46664129+owmyfeellings@users.noreply.github.com>
2025-07-30 22:12:02 +0800

    update

--- customize.sh
+++ customize.sh
@@ -10,7 +10,6 @@ pushd ./openwrt
 sed -i 's#$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.##g' ./package/lean/default-settings/files/zzz-default-settings
 popd
 
-pushd ./my-feeds
-sed -i '/luci.main.mediaurlbase/d' $(find $(find ./ -name "luci-theme-kucat" -type d) \( -name "30_luci-kucat" -or -name "30_luci-kuacat" \) -type f)
+pushd ./custom
 sed -i 's#../../lang/rust/rust-package.mk#$(TOPDIR)/feeds/packages/lang/rust/rust-package.mk#g' $(find ./smartdns -name 'Makefile' -type f)
 popd
